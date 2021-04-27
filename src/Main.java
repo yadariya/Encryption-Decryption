f84c3d23b8a73e984e896c8b63084c9bfee454ff
yadariya <66779337+yadariya@users.noreply.github.com>
2021-04-27 12:26:56 +0300

    File management is added

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,66 @@
-import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        int desired = 0;
+    public static void main(String[] args) throws IOException {
+        int desired_data = 0;
+        int desired_in = 0;
+        int desired_out = 0;
+        int desired_mode = 0;
+        int desired_key = 0;
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-data")) desired = i + 1;
+            if (args[i].equals("-data")) desired_data = i + 1;
+            if (args[i].equals("-in")) desired_in = i + 1;
+            if (args[i].equals("-out")) desired_out = i + 1;
+            if (args[i].equals("-mode")) desired_mode = i + 1;
+            if (args[i].equals("-key")) desired_key = i + 1;
         }
-        String data = args[desired];
-        String string = Arrays.toString(args).replace("[", "").replace("]", "").replace(",", "");
-        int index_mode = string.indexOf("mode");
-        String mode;
-        if (index_mode == -1) {
+        String _data = args[desired_data];
+        String file_in = args[desired_in];
+        String file_out = args[desired_out];
+        String mode = args[desired_mode];
+        String _key = args[desired_key];
+        int key = Integer.parseInt(_key);
+        String data = "";
+        if (desired_mode == 0) {
             mode = "enc";
-        } else {
-            mode = string.substring(index_mode + 5, index_mode + 8);
         }
-        int index_key = string.indexOf("key");
-        int key;
-        if (index_key == -1) {
+        if (desired_key == 0) {
             key = 0;
-        } else {
-            key = Integer.parseInt(string.substring(index_key + 4, index_key + 5));
         }
-        int index_data = string.indexOf("data");
-        if (index_data == -1) {
+        if (desired_data == 0 && desired_in == 0) {
             data = "";
+        } else if (desired_data != 0 && desired_in == 0) {
+            data = _data;
+        } else if (desired_in != 0 && desired_data == 0) {
+            File file = new File(file_in);
+            Scanner scn = new Scanner(file);
+            data = new String(Files.readAllBytes(Paths.get(file_in)));
+            scn.close();
         }
-        if (mode.equals("enc")) {
-            System.out.println(encryption(data, key));
-        } else if (mode.equals("dec")) {
-            System.out.println(decryption(data, key));
+        if (desired_out != 0) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file_out, false));
+            if (mode.equals("enc")) {
+                StringBuilder answer = encryption(data, key);
+                writer.append(answer);
+                writer.close();
+            } else if (mode.equals("dec")) {
+                StringBuilder answer = decryption(data, key);
+                writer.append(answer);
+                writer.close();
+            }
+        } else {
+            if (mode.equals("enc")) {
+                System.out.println(encryption(data, key));
+            } else if (mode.equals("dec")) {
+                System.out.println(decryption(data, key));
+            }
+
         }
     }
 
@@ -52,17 +82,5 @@ class Main {
             newString.append((char) _new);
         }
         return newString;
-    }
-
-    public static boolean isDigit(char c) {
-        String s = String.valueOf(c);
-        String digit = "1234567890";
-        return digit.contains(s);
-    }
-
-    public static boolean isSym(char c) {
-        String s = String.valueOf(c);
-        String digit = "!. ?-_%#\"+=&`~\\";
-        return digit.contains(s);
     }
 }
